952009921ead20e4ab3f53ea3e921b8b44698263
gamerbarsik <gamerbarsikd@gmail.com>
2026-01-23 21:59:52 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -10,14 +10,12 @@ app = Flask(__name__, static_folder="static")
 # === Настройка базы данных ===
 database_url = os.environ.get("DATABASE_URL")
 if database_url:
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_url.replace(
-        "postgres://", "postgresql://"
-    )
+    # Используем pg8000 вместо psycopg2
+    db_url = database_url.replace("postgres://", "postgresql+pg8000://")
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 else:
     os.makedirs("instance", exist_ok=True)
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../instance/messenger.db"
-
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
